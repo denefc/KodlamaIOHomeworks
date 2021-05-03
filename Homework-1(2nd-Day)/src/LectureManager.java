5af6f16f667da77079138824280df0e11e956de9
denefc <57840021+denefc@users.noreply.github.com>
2021-05-03 19:28:59 +0300

    Delete Homework-1(2nd-Day)/src directory

--- Homework-1(2nd-Day)/src/LectureManager.java
+++
@@ -1,8 +0,0 @@
-public class LectureManager {
-    void addToLecture(Lecture lecture){
-        System.out.println(lecture.lectureName+" added");
-    }
-    void deleteToLecture(Lecture lecture){
-        System.out.println(lecture.lectureName+" deleted");
-    }
-}
